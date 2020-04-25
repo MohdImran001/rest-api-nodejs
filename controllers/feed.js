fff573a49878e65eaaf16a30b134b6625a966d80
Mohd <imrancrazy200@gmail.com>
2020-04-25 20:19:30 +0530

    first commit

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
@@ -25,13 +28,20 @@ exports.createPost = (req, res, next) => {
         throw error;
     }
 
+    if(!req.file) {
+        const error = new Error('no image found');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
 
     const post = new Post({
         title: title,
         content: content,
-        imageUrl: "images/duck.jpg",
+        imageUrl: imageUrl,
         creator: { "name": "Mohd Imran" }
     })
     
@@ -73,4 +83,66 @@ exports.getPost = (req, res, next) => {
             err.statusCode = 500;
         next(err);
     })
+};
+
+exports.updatePost = (req, res, next) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        const error = new Error("Validation Failed, enterede data is incorrect");
+        error.statusCode = 422;
+        throw error;
+    }
+
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    const imageUrl = req.body.image;
+
+    if(req.file) {
+        imageUrl = req.file.path;
+    }
+
+    if(!imageUrl) {
+        const error = new Error('no image picked');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    Post
+    .findById(postId)
+    .then(post => {
+        if(!post) {
+            const error = new Error('no post found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        if(imageUrl !== post.imageUrl)
+            clearImage(post.imageUrl);
+
+        post.title = title;
+        post.content = content;
+        post.imageUrl = imageUrl;
+        return post.save();
+    })
+    .then(result => {
+        res
+        .status(200)
+        .send({ 
+            message: "post updated", 
+            post: result
+        });
+    })
+    .catch(err => {
+        next(err);
+    })
+
+}
+
+const clearImage = filePath => {
+        filePath = path.join(__dirname, '..', filePath);
+        fs.unlink(filePath, (err) => {
+            console.log(err);
+        });
 }
--- routes/feed.js
+++ routes/feed.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const { body } = require('express-validator');
 
 const feedCtrl = require('../controllers/feed');
@@ -12,12 +11,18 @@ Router.get('/posts', feedCtrl.getPosts);
 //POST /feed/post
 Router.post('/post', [
 	
-	bodyParser.json({ type: 'application/json' }),
 	body('title').trim().isLength({ min:5 }),
 	body('content').trim().isLength({ min:5 })
 
 ], feedCtrl.createPost);
 
 Router.get('/post/:postId', feedCtrl.getPost);
+
+Router.put('/post/:postId', [
+
+	body('title').trim().isLength({ min:5 }),
+	body('content').trim().isLength({ min:5 })
+
+],feedCtrl.updatePost);
 
 module.exports = Router;
